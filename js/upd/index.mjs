b3b545879647e4ee49adde1ef23d9132c6308a25
JRChanakarn <jrchanakarn@gmail.com>
2022-08-02 15:13:44 +0700

    try to reed jpg file and upload to firebase storage

---
+++ js/upd/index.mjs
@@ -0,0 +1,16 @@
+
+
+
+
+import { initializeApp } from "firebase/app";
+export const firebaseConfig = {
+    apiKey: "AIzaSyAhfpKgLgE553HnqUjVgQeTNrGmYaZMShU",
+    authDomain: "web-melon01.firebaseapp.com",
+    projectId: "web-melon01",
+    storageBucket: "web-melon01.appspot.com",
+    messagingSenderId: "437275991929",
+    appId: "1:437275991929:web:7af5b4e5b0f0745a8499a8",
+    measurementId: "G-RPG07G5C8X"
+};
+
+export const app = initializeApp(firebaseConfig);
